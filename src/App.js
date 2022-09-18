7933096f53798bc017b8fb8af0cda752e423693a
anikdev2016 <anikdev2016@gmail.com>
2022-09-18 20:05:21 +0600

    installed the dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-center text-6xl">React with Tailwind CSS</h1>
+
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#000000",
+
+          "secondary": "#F9F9F9",
+
+          "accent": "#F3F3F3",
+
+          "neutral": "#251E2A",
+
+          "base-100": "#fff",
+
+          "info": "#44BAD5",
+
+          "success": "#0F6732",
+
+          "warning": "#C89B09",
+
+          "error": "#F44358",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
