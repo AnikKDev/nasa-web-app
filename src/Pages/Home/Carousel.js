515a2c7de341e4c74f85af83bb769e3355a7cb83
anikdev2016 <anikdev2016@gmail.com>
2022-09-20 14:17:00 +0600

    updated data

--- src/Pages/Home/Carousel.js
+++ src/Pages/Home/Carousel.js
@@ -24,7 +24,10 @@ const Carousel = () => {
                     <div className='carousel-image-container1'>
                         <div className='p-10 md:ml-8 h-80  text-secondary   lg:w-1/2 '>
                             <h1 className="sm:text-3xl lg:text-6xl">MEOWWWWW!!!</h1>
-                            <p className="my-10 leading-8 lg:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quidem, ratione quaerat, iste dolor harum, esse laudantium autem consequuntur dolorem consequatur labore enim deleniti nesciunt totam cum. Enim, earum impedit!</p>
+                            <p className="my-10 leading-8 lg:text-xl">“Floods, droughts, heatwaves, extreme storms and wildfires are going from bad to worse, breaking records with alarming frequency. Heatwaves in Europe. Colossal floods in Pakistan. Prolonged and severe droughts in China, the Horn of Africa and the United States.</p>
+
+                            <p className='italic'>"There is nothing natural about the new scale of these disasters. They are the price of humanity’s fossil fuel addiction,”</p>
+                            <p className='my-2'>- UN Secretary-General António Guterres.</p>
                         </div>
                     </div>
                 </SwiperSlide>
@@ -41,7 +44,7 @@ const Carousel = () => {
 
                         <div className='p-10 md:ml-8 h-80  text-secondary   lg:w-1/2 '>
                             <h1 className="sm:text-3xl lg:text-6xl">MEOWWWWW!!!</h1>
-                            <p className="my-10 leading-8 lg:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quidem, ratione quaerat, iste dolor harum, esse laudantium autem consequuntur dolorem consequatur labore enim deleniti nesciunt totam cum. Enim, earum impedit!</p>
+                            <p className="my-10 leading-8 lg:text-xl">Polar ice caps are melting as global warming causes climate change. We lose Arctic sea ice at a rate of almost 13% per decade, and over the past 30 years, the oldest and thickest ice in the Arctic has declined by a stunning 95%.</p>
                         </div>
                     </div>
                 </SwiperSlide>
